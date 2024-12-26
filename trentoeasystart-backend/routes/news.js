e1d8f87ba54bf81aaafbb700e650b76d76bd9221
Ismaele <tiados04@gmail.com>
2024-12-26 23:15:05 +0100

    fix: deleted unsed directories

--- trentoeasystart-backend/routes/news.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getNews } = require('../controllers/newsController');
-
-router.get('/', getNews);
-
-module.exports = router;
