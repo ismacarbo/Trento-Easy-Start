f230327e25d4c02223ba07dca9285cd28ef031a2
Ismaele <tiados04@gmail.com>
2024-12-26 23:06:58 +0100

    added accomodations & auth routes

--- trentoeasystart-backend/routes/services.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getServices } = require('../controllers/servicesController');
-
-router.get('/', getServices);
-
-module.exports = router;
